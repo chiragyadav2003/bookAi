b232a4901b8484331072b7dc4f624178526cfa0a
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-08-16 22:59:16 +0530

    integrate in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import PricingSection from './components/PricingSection';
-import { ThemeProvider } from './contexts/ThemeContext';
+import { LightParticleBackground } from './components/lightParticleBackground';
+import { DarkParticleBackground } from './components/darkParticleBackground';
+import { useTheme } from './contexts/ThemeContext';
 
 const App: React.FC = () => {
+  const { isDarkMode } = useTheme();
   return (
-    <ThemeProvider>
-      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-200">
-        <Navbar />
-        <main className="py-12 px-4 sm:px-6 lg:px-8">
-          <PricingSection />
-        </main>
+    <>
+      <div className="relative min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-200">
+        {
+          isDarkMode ? <DarkParticleBackground /> : <LightParticleBackground />
+        }
+        <div className="relative z-10"> {/* Ensure z-index is higher here */}
+          <Navbar />
+          <main className="py-12 px-4 sm:px-6 lg:px-8">
+            <PricingSection />
+          </main>
+        </div>
       </div>
-    </ThemeProvider>
+    </>
   );
 };
 

a98d3bb6f96576d2ae0bb9d61e8300e1de3635e1
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-08-16 21:06:19 +0530

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="h-full">
+      BookAI pricing section
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6D28D9',
+        secondary: '#4C1D95',
+        background: '#1F2937',
+      },
+    },
+  },
+  plugins: [],
+};
